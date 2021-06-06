e8b691dbcad47bb0c0bb1f3ff9b4f9d191e34f74
salnavs <52954982+salnavs@users.noreply.github.com>
2021-06-06 23:16:03 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,6 +3,42 @@
 #include <stdio.h>
 #include <math.h>
 
+float mc_pi(int);
+
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
+
+int main(void) {
+  float pi0;
+  float pi1;
+
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+
+
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+  }
+}
+
 float mc_pi(int n){
 	int i=0;
         int circle_ct=0;
@@ -22,5 +58,4 @@ float mc_pi(int n){
 	 float pi=4*(float)circle_ct/(total_ct);
 	 return (pi);
 	}
-
 
